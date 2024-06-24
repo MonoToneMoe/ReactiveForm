c3d963dde025c266f940b7db44a6accdbe173dc0
MonoToneMoe <gghaddadgg@gmail.com>
2024-06-24 10:59:14 -0700

    created user form and checked for validation

--- src/app/user-form/user-form.component.spec.ts
+++ src/app/user-form/user-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserFormComponent } from './user-form.component';
+import { CommonModule } from '@angular/common';
 
 describe('UserFormComponent', () => {
   let component: UserFormComponent;
@@ -8,7 +9,7 @@ describe('UserFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserFormComponent]
+      imports: [UserFormComponent, CommonModule]
     })
     .compileComponents();
 
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function dateValidator(control: FormControl): { [key: string]: boolean } | null {
+  // Get today's date
+  const today = new Date();
+
+  // Selected date from form control
+  const selectedDate = new Date(control.value);
+
+  // Compare dates
+  if (selectedDate > today) {
+    return { 'futureDate': true }; // Return validation error if date is in future
+  }
+  return null; // No validation error
+}
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
 
+  userForm!: FormGroup;
+  constructor() { }
+
+  ngOnInit() {
+    this.userForm = new FormGroup({
+      fName: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+      lName: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      date: new FormControl('', [Validators.required, dateValidator]),
+      address: new FormControl(''),
+      phone: new FormControl(''),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required)
+    });
+  }
 }
