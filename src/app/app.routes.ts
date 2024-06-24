6fb7b71361f4ed186949814c4b45f6a73e04d4cd
MonoToneMoe <gghaddadgg@gmail.com>
2024-06-24 10:58:25 -0700

    update routes to include user form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserFormComponent } from './user-form/user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'userform', pathMatch: 'full' },
+    { path: 'userform', component: UserFormComponent },
+  ];
