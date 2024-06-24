1c2766f9f680783571c15aa4bd70ba7185c15dc9
MonoToneMoe <gghaddadgg@gmail.com>
2024-06-24 10:59:38 -0700

    connected user form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewsModule } from './views/views.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ViewsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserFormComponent } from '../user-form/user-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    UserFormComponent,
+    ReactiveFormsModule
   ]
 })
 export class ViewsModule { }
