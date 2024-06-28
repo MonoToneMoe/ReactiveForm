f4e5b9b9fd2e5ae376854a90cb6beecbd426574d
MonoToneMoe <gghaddadgg@gmail.com>
2024-06-28 15:04:15 -0700

    commiting

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators, FormBuilder } from '@angular/forms';
 import { initFlowbite } from 'flowbite';
+import { DataService } from '../data.service';
+import { IAddUser, IGetAllUsers } from '../interfaces';
 
 function dateValidator(control: FormControl): { [key: string]: boolean } | null {
   const today = new Date();
@@ -31,10 +33,9 @@ function passwordValidator(control: FormControl): { [key: string]: boolean } | n
   return null;
 }
 
-function passwordMatchValidator(control: FormGroup): ValidationErrors | null {
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
   const password = control.get('password');
   const confirmPassword = control.get('confirmPassword');
-
   if (password && confirmPassword && password.value !== confirmPassword.value) {
     return { 'passwordMismatch': true };
   }
@@ -50,22 +51,28 @@ function passwordMatchValidator(control: FormGroup): ValidationErrors | null {
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent implements OnInit {
+  fetchData: IGetAllUsers[] | undefined
 
   userForm!: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getAllUsers().subscribe((data: IGetAllUsers[]) => {
+      this.fetchData = data;
+      console.log(this.fetchData);
+    });
     this.userForm = this.formBuilder.group({
       fName: ['', [Validators.required, Validators.maxLength(100)]],
       lName: ['', [Validators.required, Validators.maxLength(100)]],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       date: ['', [Validators.required, dateValidator]],
       address: ['', Validators.maxLength(100)],
       phone: ['', phoneValidator],
       passwordGroup: this.formBuilder.group({
         password: ['', Validators.required, Validators.minLength(15), passwordValidator],
         confirmPassword: ['', Validators.required]
-      }, { validator: passwordMatchValidator })
+      }, { validator: passwordMatchValidator }),
+      isAdmin: [false]
     });
   };
 
@@ -96,12 +103,42 @@ export class UserFormComponent implements OnInit {
   }
 
   submitForm() {
-    if(this.userForm.invalid) {
+    if (this.userForm.invalid) {
+      this.markFormGroupTouched(this.userForm);
       alert('Fix errors on form');
     } else {
-      alert('Success');
-        console.log(this.userForm.value);
-      this.userForm.reset();
+      const formData: IAddUser = {
+        firstName: this.userForm.value.fName,
+        lastName: this.userForm.value.lName,
+        email: this.userForm.value.email,
+        birthday: this.userForm.value.date,
+        address: this.userForm.value.address,
+        phoneNumber: this.userForm.value.phone,
+        password: this.userForm.value.passwordGroup.password,
+        isAdmin: this.userForm.value.isAdmin
+      };
+      console.log(formData)
+
+      this.dataService.AddUser(formData).subscribe({
+        next: (response) => {
+          alert('User added successfully');
+          console.log(response);
+          this.userForm.reset();
+        },
+        error: (error) => {
+          alert('There was an error adding the user');
+          console.error(error);
+        }
+      });
     }
+  }
+  
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach((control: AbstractControl) => {
+      control.markAsTouched();
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
   }
 }
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserFormComponent } from '../user-form/user-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
 
 
 
@@ -11,6 +12,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     UserFormComponent,
     ReactiveFormsModule
-  ]
+  ],
+  providers: [provideHttpClient()]
 })
 export class ViewsModule { }
