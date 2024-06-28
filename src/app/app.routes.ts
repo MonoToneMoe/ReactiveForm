212a45107d309c71afcf8f89400e53d3dee57df7
TerryKrabs <thegoldenlemon1@gmail.com>
2024-06-28 16:49:12 -0700

    EOD friday 6/28 -xavier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { UserFormComponent } from './user-form/user-form.component';
+import { LandingComponent } from './landing/landing.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'userform', pathMatch: 'full' },
     { path: 'userform', component: UserFormComponent },
+    {path: 'landing', component: LandingComponent}
   ];
