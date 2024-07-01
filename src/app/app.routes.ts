bd0d9f0c4674eb2716e95eb8311acc34415638c3
JosephPoncini <jxponcini@gmail.com>
2024-07-01 08:45:19 -0700

    user management and student directory initial pages added and included in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { UserFormComponent } from './user-form/user-form.component';
 import { LoginComponent } from './login/login.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { StudentDirectoryComponent } from './student-directory/student-directory.component';
+import { UserManagementComponent } from './user-management/user-management.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'userform', pathMatch: 'full' },
     { path: 'userform', component: UserFormComponent },
     { path: 'login', component: LoginComponent },
     { path: 'reset-password', component: ResetPasswordComponent },
+    { path: 'student-directory', component: StudentDirectoryComponent },
+    { path: 'user-management', component: UserManagementComponent },
   ];
---
+++ src/app/student-directory/student-directory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-directory',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-directory.component.html',
+  styleUrl: './student-directory.component.css'
+})
+export class StudentDirectoryComponent {
+
+}
