f57f71f83c21770036303f0d857b61f937c073a5
MonoToneMoe <gghaddadgg@gmail.com>
2024-07-01 16:56:49 -0700

    push to repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
 import { StudentDirectoryComponent } from './student-directory/student-directory.component';
 import { UserManagementComponent } from './user-management/user-management.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'userform', pathMatch: 'full' },
     { path: 'userform', component: UserFormComponent },
+    { path: 'signup', component: RegistrationComponent },
     { path: 'login', component: LoginComponent },
     { path: 'reset-password', component: ResetPasswordComponent },
     { path: 'student-directory', component: StudentDirectoryComponent },
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
 
+  signupForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder, private dataService: DataService) { }
+
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(15), passwordValidator]],
+      passwordGroup: this.formBuilder.group({
+        password: ['', Validators.required, Validators.minLength(15), passwordValidator],
+        confirmPassword: ['', Validators.required]
+      }, { validator: passwordMatchValidator }),
+      address: ['', [Validators.maxLength(100)]],
+      phoneNumber: ['', [phoneValidator]],
+      firstname: ['', [Validators.required, Validators.maxLength(100)]],
+      lastName: ['', [Validators.required, Validators.maxLength(100)]],
+      birthDay: ['', [Validators.required, dateValidator]],
+      isAdmin: [false]
+    })
+  }
+}
+
+function passwordValidator(control: FormControl): { [key: string]: boolean } | null {
+  const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*\d)(?=.*[?!@#$%^&*])[A-Za-z\d?!@#$%^&*]{15,}$/;
+  if (!PASSWORD_REGEX.test(control.value)) {
+    return { 'invalidPassword': true };
+  }
+  return null;
+}
+function passwordMatchValidator(control: FormGroup): ValidationErrors | null {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  if (password && confirmPassword && password.value !== confirmPassword.value) {
+    return { 'passwordMismatch': true };
+  }
+  return null;
+}
+function phoneValidator(control: any): { [key: string]: boolean } | null {
+  const PHONE_REGEX = /^\(\d{3}\) \d{3}-\d{4}$/;
+  if (!PHONE_REGEX.test(control.value)) {
+    return { 'invalidPhone': true };
+  }
+  return null;
+}
+
+function dateValidator(control: FormControl): { [key: string]: boolean } | null {
+  const today = new Date();
+
+  const selectedDate = new Date(control.value);
+
+  // Compare dates
+  if (selectedDate > today) {
+    return { 'futureDate': true };
+  }
+  return null;
 }
