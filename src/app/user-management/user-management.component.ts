b3d743239641f2e98dd6d7fb35de6be355d93fa5
TerryKrabs <thegoldenlemon1@gmail.com>
2024-07-02 14:57:54 -0700

    RARRR

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule, NgFor } from '@angular/common';
+import { DataService } from '../data.service';
+import { Student } from '../interfaces';
 
 @Component({
   selector: 'app-user-management',
   standalone: true,
-  imports: [],
+  imports: [NgFor, CommonModule, NavbarComponent],
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.css'
 })
-export class UserManagementComponent {
+export class UserManagementComponent implements OnInit {
+
+  dataService = inject(DataService);
+  studentList: Student[] | null = null 
+
+
+  loadStudentData() {
+    this.dataService.getStudentData().subscribe((data) => {
+      // console.log(list);
+      this.studentList = data;
+      console.log(this.studentList)
+    });
+  }
+
+  ngOnInit() {
+    this.loadStudentData();
+  }
 
 }
